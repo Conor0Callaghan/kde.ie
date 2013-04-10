b45a168c08f0e9b81da98f79f53fb3b0086f6510
Conor O'Callaghan <brioscaibriste@gmail.com>
2013-04-10 12:52:19 +0100

    adding in correct facebook page URL

--- website/footer.php
+++ website/footer.php
@@ -7,7 +7,7 @@ print <<<HTML
 	
 	<!-- Social media links -->
 
-	<div class="fb-like" data-send="true" data-layout="button_count" data-width="450" data-show-faces="false"></div> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <div class="fb-follow" data-href="https://www.facebook.com/TORTASINESROSALES" data-layout="button_count" data-show-faces="false" data-width="450"></div>
+	<div class="fb-like" data-send="true" data-layout="button_count" data-width="450" data-show-faces="false"></div> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <div class="fb-follow" data-href="https://www.facebook.com/pages/KDE-Ireland/147864812055556" data-layout="button_count" data-show-faces="false" data-width="450"></div>
 	<br>
 
 	KDE, K Desktop Environment and the KDE Logo are trademarks of <a href="http://ev.kde.org/" target="blank">KDE e.V.</a>
